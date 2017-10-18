5e64ae1d832a59d6a722efb1cd8c161531cb4404
Andrew Horn <arhorn@smcm.edu>
2017-10-18 21:14:29 +0700

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
   // validate id using isValid
     // 404 - send back empty body
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    res.status(404).send('id not valid');
   }
 
   // findById
@@ -46,12 +46,12 @@ app.get('/todos/:id', (req, res) => {
       // if no todo - send back 404 with empty body
     // error
       // 400 - and send empty body back
-  Todo.findById(id).then(({todo}) => {
+  Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(404).send();
+      return res.status(404).send('id not found');
     }
-    res.send(todo);
-  }).catch((e) => res.status(400).send())
+    res.send({todo});
+  }).catch((e) => res.status(400).send('other error'))
 });
 
 app.listen(3000, () => {
