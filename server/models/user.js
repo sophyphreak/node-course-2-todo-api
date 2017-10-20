05135f131936362cc11fd4b362597b8818eb93ff
Andrew Horn <arhorn@smcm.edu>
2017-10-21 02:26:54 +0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (!res) {
+          reject();
+        }
+        resolve(user);
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
     // user.password
